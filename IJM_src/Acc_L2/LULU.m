e87b1e62613ba9d47069361d852b86c09f4dc096
ijmaywar <ijmaywar@gmail.com>
2023-09-29 19:34:47 -0400

    Update L2 codes
    Updated LULUmini code to work thru GD instead of external HD. Changed LULUmini to LULU and deleted LULU and some weird filecomparison variable I had saved a long time ago

--- IJM_src/Acc_L2/LULU.m
+++ IJM_src/Acc_L2/LULU.m
@@ -1,6 +1,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % Flap Detection LULU
+% This code counts number of flaps in five minute windows to match GPS
+% data's resolution.
 % Ian Maywar
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -15,24 +17,24 @@ location = 'Bird_Island'; % Options: 'Bird_Island', 'Midway', 'Wandering'
 
 %% Set envrionment
 
-s4_dir = strcat('/Volumes/LaCie/L1/Bird_Island/Tag_Data/Accelerometer/Acc_Technosmart/',szn,'/s4_Trimmed_Untilted/');
-L2_dir = strcat('/Volumes/LaCie/L2/Bird_Island/Tag_Data/Accelerometer/Acc_Technosmart/',szn,'/');
+% set directories
+GD_dir = '/Users/ian/Library/CloudStorage/GoogleDrive-ian.maywar@stonybrook.edu/.shortcut-targets-by-id/1-mLOKt79AsOpkCFrunvcUj54nuqPInxf/THORNE_LAB/Data/Albatross/NEW_STRUCTURE/';
+L1_dir = strcat(GD_dir,'L1/',location,'/Tag_Data/Accelerometer/Acc_Technosmart/',szn,'/');
+L2_dir = strcat(GD_dir,'L2/',location,'/Tag_Data/Accelerometer/',szn,'/');
 %L2_dir = strcat('/Volumes/LaCie/L2/Bird_Island/Tag_Data/Accelerometer/Acc_Technosmart/',szn,'_update/');
 
 % Matlab functions toolbox
-addpath(genpath('/Users/ian/Documents/ThorneLab_FlightDynamics/'))
-addpath(genpath('/Users/ian/Library/CloudStorage/GoogleDrive-ian.maywar@stonybrook.edu/Other computers/My MacBook Pro (1)/ThorneLab_FlightDynamics/IJM_src'))
-addpath(genpath('/Users/ian/Library/CloudStorage/GoogleDrive-ian.maywar@stonybrook.edu/Other computers/My MacBook Pro (1)/ThorneLab_FlightDynamics/3rd_party_functions'))
+addpath(genpath('/Users/ian/Documents/GitHub/AlbatrossFlightDynamics/'))
 
 % Full_metadata sheet
-fullmeta = readtable('/Volumes/LaCie/Full_metadata.xlsx','Sheet',location,'TreatAsEmpty',{'NA'});
+fullmeta = readtable(strcat(GD_dir,'metadata/Full_metadata.xlsx'));
 % Specify the field season and location you are interested in
 fullmeta = fullmeta(strcmp(fullmeta.Field_season,szn) & strcmp(fullmeta.Location,location),:);
 
-cd(s4_dir)
-s4_fileList = exFAT_aux_remove(struct2table(dir('*.txt')));
+cd(L1_dir)
+L1_fileList = exFAT_aux_remove(struct2table(dir('*.csv')));
 
-disp(CheckMeta(fullmeta,s4_fileList,3,"Deployment_ID"))
+disp(CheckMeta(fullmeta,L1_fileList,3,"Deployment_ID"))
 
 % Get monitor dimensions to display figures
 p = get(0, "MonitorPositions");
@@ -44,24 +46,23 @@ param_tbl = readtable(strcat(L2_dir,"ParameterSearch.csv"));
 set(0,'DefaultFigureVisible','off')
 
 %% Loop thru birds
-for j=1:5
-%parfor j= 6:10%height(s4_fileList)
+parfor j = 6:height(L1_fileList)
 
     %% Read bird 
-    bird = strsplit(s4_fileList.name{j},"_");
+    bird = strsplit(L1_fileList.name{j},"_");
     bird = strcat(bird{1},"_",bird{2},"_",bird{3});
-    Acc = readtable(s4_fileList.name{j},'Delimiter',',','ReadVariableNames',true,'Format','auto','TreatAsEmpty',{'NA'}); % read bird i's file
+    Acc = readtable(L1_fileList.name{j},'Delimiter',',','ReadVariableNames',true,'Format','auto','TreatAsEmpty',{'NA'}); % read bird i's file
     %%% read the accelerometer 'Az' data as a list of values
     raw = [Acc.Az];
-%%
-  for k=0:1
-%%    
-    figdir = strcat('Figures/',bird,'/k_',string(k),'/');
+   
+    figdir = strcat('Figures/',bird,'/');
     mkdir(strcat(L2_dir,figdir))
+
+    meta = struct;
     
     %% Choosing values
     m = 8; % Should be 25/3 so that it's 3 Hz. But this is equivalent to m = 8 (can't get finer than one sample).
-    % k = 0; % Remove downward signals of one sample.
+    k = 0; % Remove downward signals of one sample.
     minPk = 4; % Guess that peaks 4 samples or less apart are not flaps. Kind of arbitrary but most movingslope almost always returns 4.
 
     filtered = Ukm(raw,k,m); % 1 is 25 Hz and (25/3) is 3 Hz.
@@ -75,7 +76,7 @@ for j=1:5
     num_flaps_th = [];
     mean_pk_th = [];
 
-    parfor i = 1:length(th_range)
+    for i = 1:length(th_range)
         th_i = th_range(i);
 
         pks = findpeaks(-filtered,'MinPeakHeight',-th_i,'MinPeakDistance',minPk);
@@ -95,7 +96,7 @@ for j=1:5
     num_flaps_m = [];
     mean_pk_m = [];
 
-    parfor i = 1:length(m_range)
+    for i = 1:length(m_range)
         m_i = m_range(i);
         filtered = Ukm(raw,k,m_i);
         pks = findpeaks(-filtered,'MinPeakHeight',-th,'MinPeakDistance',minPk);
@@ -109,25 +110,25 @@ for j=1:5
     [~,m_idx] = min(mvSlp_m);
     m = m_range(m_idx);
 
-    % Search for MinPeakDistance
+    % % Search for MinPeakDistance
 
-    minPk_range = (k:7); % k is the lower end because downward pulses k and smaller are annihilated. 
-    num_flaps_minPk = [];
-    mean_pk_minPk = [];
-
-    parfor i = 1:length(minPk_range)
-        minPk_i = minPk_range(i);
-        filtered = Ukm(raw,k,m);
-        pks = findpeaks(-filtered,'MinPeakHeight',-th,'MinPeakDistance',minPk_i);
-        pks = -pks; % Flip the peaks upsidedown
-    
-        num_flaps = length(pks);
-        num_flaps_minPk(i) = num_flaps;
-    end
-
-    mvSlp_minPk = abs(movingslope(num_flaps_minPk));
-    [~,minPk_idx] = min(mvSlp_minPk);
-    minPk = minPk_range(minPk_idx);
+    % minPk_range = (k:7); % k is the lower end because downward pulses k and smaller are annihilated. 
+    % num_flaps_minPk = [];
+    % mean_pk_minPk = [];
+    % 
+    % parfor i = 1:length(minPk_range)
+    %     minPk_i = minPk_range(i);
+    %     filtered = Ukm(raw,k,m);
+    %     pks = findpeaks(-filtered,'MinPeakHeight',-th,'MinPeakDistance',minPk_i);
+    %     pks = -pks; % Flip the peaks upsidedown
+    % 
+    %     num_flaps = length(pks);
+    %     num_flaps_minPk(i) = num_flaps;
+    % end
+    % 
+    % mvSlp_minPk = abs(movingslope(num_flaps_minPk));
+    % [~,minPk_idx] = min(mvSlp_minPk);
+    % minPk = minPk_range(minPk_idx);
 
     %% Plot figures and save
 
@@ -163,21 +164,21 @@ for j=1:5
     xlabel('m')
     saveas(nFlaps_movslp_m,strcat(L2_dir,figdir,bird,'_search_m.fig'))
 
-    % minPkDistance
-
-    nFlaps_movslp_minPk = figure;
-    yyaxis left
-        scatter(minPk_range,num_flaps_minPk)
-        hold on 
-        scatter(minPk_range(minPk_idx),num_flaps_minPk(minPk_idx),'LineWidth',5)
-        ylabel('Number of flaps')
-    yyaxis right
-        scatter(minPk_range,mvSlp_minPk)
-        hold on
-        scatter(minPk_range(minPk_idx),mvSlp_minPk(minPk_idx),'LineWidth',5)
-        ylabel('movingslope')
-    xlabel('Minimum Peak Distance')
-    saveas(nFlaps_movslp_minPk,strcat(L2_dir,figdir,bird,'_search_minPk.fig'))
+    % % minPkDistance
+    % 
+    % nFlaps_movslp_minPk = figure;
+    % yyaxis left
+    %     scatter(minPk_range,num_flaps_minPk)
+    %     hold on 
+    %     scatter(minPk_range(minPk_idx),num_flaps_minPk(minPk_idx),'LineWidth',5)
+    %     ylabel('Number of flaps')
+    % yyaxis right
+    %     scatter(minPk_range,mvSlp_minPk)
+    %     hold on
+    %     scatter(minPk_range(minPk_idx),mvSlp_minPk(minPk_idx),'LineWidth',5)
+    %     ylabel('movingslope')
+    % xlabel('Minimum Peak Distance')
+    % saveas(nFlaps_movslp_minPk,strcat(L2_dir,figdir,bird,'_search_minPk.fig'))
 
     %% Final calculation
 
@@ -186,6 +187,8 @@ for j=1:5
     pks = -pks; % Flip the peaks upsidedown
 
     num_flaps = length(pks);
+
+    pk_nearest = nearestPeak(pk_locs);
 
     %% Save filtered figure (first 2000 samples)
 
@@ -197,133 +200,122 @@ for j=1:5
     % xline(pk_locs(1:30))
     saveas(filt_fig,strcat(L2_dir,figdir,bird,'_filtered.fig'))
 
-    %% Save parameters
+    %% Save flapping data and parameters
 
-    if k==0
-        th0 = th;
-        m0 = m;
-        minPk0 = minPk;
-        num_flaps0 = num_flaps;
+    meta.bird = bird;
+    meta.th = th;
+    meta.m = m;
+    meta.minPk = minPk;
+    meta.num_flaps = num_flaps;
+    meta.data_duration_hrs = hours(Acc.DateTime(end) - Acc.DateTime(1));
+    meta.fph = num_flaps/hours(Acc.DateTime(end) - Acc.DateTime(1));
+    parsave(meta,strcat(L2_dir,'Parameters/',bird,'_Params.mat'));
 
-        filtered0 = filtered;
-        pks0 = pks;
-        pk_locs0 = pk_locs;
-        pk_w0 = pk_w;
-        pk_p0 = pk_p;
+    % Save a file that gives flap statistics for every flap
+    flap_tbl = table(Acc.DateTime(pk_locs),pks,pk_locs,pk_w,pk_p,pk_nearest,'VariableNames', {'DateTime','pks','pk_locs','pk_w','pk_p','pk_nearest'});
+    writetable(flap_tbl,strcat(L2_dir,bird,"_Acc_L2.csv"))
 
-    else
-        th1 = th;
-        m1 = m;
-        minPk1 = minPk;
-        num_flaps1 = num_flaps;
-
-        filtered1 = filtered;
-        pks1 = pks;
-        pk_locs1 = pk_locs;
-        pk_w1 = pk_w;
-        pk_p1 = pk_p;
-    end
-  end
-
-    %% Seperation of peaks
-
-    diffPk_locs0 = diff(pk_locs0);
-    Pk_dists0(1) = diffPk_locs0(1);
-    for i=2:length(diffPk_locs0)
-        Pk_dists0(i) = max(diffPk_locs0(i),diffPk_locs0(i-1));
-    end
-    Pk_dists0(end+1) = diffPk_locs0(end);
-
-    diffPk_locs1 = diff(pk_locs1);
-    Pk_dists1(1) = diffPk_locs1(1);
-    for i=2:length(diffPk_locs1)
-        Pk_dists1(i) = max(diffPk_locs1(i),diffPk_locs1(i-1));
-    end
-    Pk_dists1(end+1) = diffPk_locs1(end);
-
-    %% Histogram of peak distances and heights
-
-    peak_dist_fig = figure;
-    subplot(3,2,1)
-        h0 = histogram(Pk_dists0,'BinEdges',(5:1:35));
-        title('k=0')
-        YL = get(gca, 'YLim');
-        ylim(YL)
-    subplot(3,2,3)
-        h1 = histogram(Pk_dists1,'BinEdges',(5:1:35));
-        title('k=1')
-        ylim(YL)
-    subplot(3,2,5)
-        histogram('BinCounts',h0.BinCounts ./ h1.BinCounts,'BinEdges',(5:1:35));
-        title('ratio')
-        YL = get(gca, 'YLim');
-        ylim([1 YL(2)])
-    subplot(3,2,2)
-        h0 = histogram(-pks0,'BinEdges',(1:.2:4));
-        title('k=0')
-        YL = get(gca, 'YLim');
-        ylim(YL)
-    subplot(3,2,4)
-        h1 = histogram(-pks1,'BinEdges',(1:.2:4));
-        title('k=1')
-        ylim(YL)
-    subplot(3,2,6)
-        histogram('BinCounts',h0.BinCounts ./ h1.BinCounts,'BinEdges',(1:.2:4));
-        title('ratio')
-
-
-    % saveas(peak_dist_fig,strcat(L2_dir,figdir,bird,'_PeakDists.fig'))
-
-    %% Save th, m , MinPeakDistance
-
-    param_tbl.bird(param_tbl.j==j) = {bird};
-    param_tbl.th(param_tbl.j==j) = th;
-    param_tbl.m(param_tbl.j==j) = m;
-    param_tbl.minPk(param_tbl.j==j) = minPk;
-    param_tbl.num_flaps(param_tbl.j==j) = num_flaps;
-    param_tbl.k1_PecFlapsLost(param_tbl.j==j) = (num_flaps-num_flaps_k1)/num_flaps;
-    param_tbl.data_duration_hrs(param_tbl.j==j) = hours(Acc.DateTime(end) - Acc.DateTime(1));
-    param_tbl.flaps_per_hour(param_tbl.j==j) = num_flaps/hours(Acc.DateTime(end) - Acc.DateTime(1));
-
-    writetable(param_tbl,strcat(L2_dir,"ParameterSearch.csv"))
-        
 end
 
 
 
 
-% % %
-% % 
-% % figure
-% % plot(Acc.Ax)
-% % hold on 
-% % plot(Acc.Ay)
-% % plot(Acc.Az)
-% % 
-% % %
-% % 
-%%
-figure
-xaxis = (6347000:6349000);
-plot(xaxis,raw(xaxis))
-hold on
-plot(xaxis,filtered0(xaxis))
-yline(th0)
-xline(6347335)
-% % 
-% % 
-% % %
-% % 
-% % filtered = Ukm(raw,1,8);
-% % filtered = U_n(raw,2);
-% % figure
-% % plot(raw(1:2000))
-% % hold on
-% % xline()
-% % plot(filtered(1:2000))
-% % 
-% % 
-% % % Some stats
-% % max(diff(pk_locs))/(25*60) % max time between flaps in minutes.
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% Extra code for comparison of peaks
+% 
+% diffPk_locs0 = diff(pk_locs0);
+% Pk_dists0(1) = diffPk_locs0(1);
+% for i=2:length(diffPk_locs0)
+%     Pk_dists0(i) = max(diffPk_locs0(i),diffPk_locs0(i-1));
+% end
+% Pk_dists0(end+1) = diffPk_locs0(end);
+% 
+% diffPk_locs1 = diff(pk_locs1);
+% Pk_dists1(1) = diffPk_locs1(1);
+% for i=2:length(diffPk_locs1)
+%     Pk_dists1(i) = max(diffPk_locs1(i),diffPk_locs1(i-1));
+% end
+% Pk_dists1(end+1) = diffPk_locs1(end);
+% 
+% % Histogram of peak distances and heights
+% 
+% figdir = strcat('Figures/',bird,'/');
+% 
+% peak_dist_fig = figure;
+% subplot(3,2,1)
+%     h0 = histogram(Pk_dists0,'BinEdges',(5:1:35));
+%     h0Bins = h0.BinCounts;
+%     bar(5:1:34,h0Bins)
+%     title('k=0')
+%     ylabel('num flaps')
+%     xlabel('distance to closest peak')
+%     YL = get(gca, 'YLim');
+%     ylim(YL)
+% subplot(3,2,3)
+%     h1 = histogram(Pk_dists1,'BinEdges',(5:1:35));
+%     h1Bins = h1.BinCounts;
+%     bar(5:1:34,h1Bins)
+%     title('k=1')
+%     ylabel('num flaps')
+%     xlabel('distance to closest peak')
+%     ylim(YL)
+% subplot(3,2,5)
+%     bar(5:1:34,(h0Bins-h1Bins)./h0Bins);
+%     title('comaprison')
+%     ylabel('% change')
+%     xlabel('distance to closest peak')
+%     ylim([-1 1])
+% subplot(3,2,2)
+%     h0 = histogram(-pks0,'BinEdges',(1:.2:4));
+%     h0Bins = h0.BinCounts;
+%     bar(1:0.2:3.8,h0Bins)
+%     title('k=0')
+%     ylabel('num flaps')
+%     xlabel('peak height')
+%     YL = get(gca, 'YLim');
+%     ylim(YL)
+% subplot(3,2,4)
+%     h1 = histogram(-pks1,'BinEdges',(1:.2:4));
+%     h1Bins = h1.BinCounts;
+%     bar(1:0.2:3.8,h1Bins)
+%     title('k=1')
+%     ylabel('num flaps')
+%     xlabel('peak height')
+%     ylim(YL)
+% subplot(3,2,6)
+%     bar(1:0.2:3.8,(h0Bins-h1Bins)./h0Bins);
+%     title('comparison')
+%     ylabel('% change')
+%     xlabel('peak height')
+%     ylim([-1 1])
+% 
+% saveas(peak_dist_fig,strcat(L2_dir,figdir,bird,'_FlapChanges.fig'))
 
