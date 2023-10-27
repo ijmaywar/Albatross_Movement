92cf84be8d074febc46722b9042c88f962b000b0
ijmaywar <ijmaywar@gmail.com>
2023-10-27 19:35:55 -0400

    Update L0_HRL_convertToTxt.m

--- IJM_src/HRL_L0/L0_HRL_convertToTxt.m
+++ IJM_src/HRL_L0/L0_HRL_convertToTxt.m
@@ -15,16 +15,18 @@ computer = "MacMini";
 
 %% Set Environment
 
+% Matlab functions toolbox
+addpath(genpath('/Users/ian/Documents/GitHub/AlbatrossFlightDynamics/'))
+
 % set directories
 GD_dir = findGD(computer);
-L0_dir = strcat(GD_dir,"L0/",location,"/Tag_Data/",szn,"/HRL/L0_0_Raw_Compressed_dat/");
-L1_Acc_dir = strcat(GD_dir,"L1/",location,"/Tag_Data/Acc/Acc_HRL/",szn,"/");
-L1_ECG_dir = strcat(GD_dir,"L1/",location,"/Tag_Data/ECG/",szn,"/");
+L0_0_dir = strcat(GD_dir,"L0/",location,"/Tag_Data/",szn,"/Aux/HRL/L0_0_Raw/");
+L0_1_dir = strcat(GD_dir,"L0/",location,"/Tag_Data/",szn,"/Aux/HRL/L0_1_Decompressed/");
 
 % Matlab functions toolbox
 addpath(genpath('/Users/ian/Documents/GitHub/AlbatrossFlightDynamics/'))
 
-cd(L0_dir)
+cd(L0_0_dir)
 fileList = dir('*.dat'); 
 
 %% Unpack Raw Neurologger .dat files
@@ -33,38 +35,31 @@ for i= 1:length(fileList)
     
     namesplit = strsplit(fileList(i).name,'_');
     current_bird = strcat(namesplit{1},'_',namesplit{2},'_',namesplit{3});
-    % FileOutputName = [pwd filesep 'OutputFolder' filesep FileName];
     % bird = fileList(i).name(1:8);
     %     
     % set parameters-----------------------------------------------------------
-    Parameters.NChannels = 1;  %number of electrophysiological channels
+    Parameters.NChannels = 1;  % number of electrophysiological channels
     Parameters.sps = 600;     % Hz , was 1600
-    Parameters.Ku = 166.6;    %input range +/-6 mV
+    Parameters.Ku = 166.6;    % input range +/-6 mV
     Parameters.Coef = 2000/Parameters.Ku/1024;    %2V, 10 bit probably +/-0.5mV range
     
     
     % Import data--------------------------------------------------------------
-    [Calibration, EEG_t, Accel_t, Pressure_t, Temp_t, Gyro_t, Magn_t, Sync_t,HeaderPosition] = Datalogger4ChConverter_MARG_Altimeter_final([FileName '.dat'], Parameters);
+    [Calibration, ECG_t, Accel_t, Pressure_t, Temp_t, Gyro_t, Magn_t, Sync_t,HeaderPosition] = Datalogger4ChConverter_MARG_Altimeter_final(fileList(i).name, Parameters);
     
-    
-    %Write header positions in the text file-----------------------------------
-    % fileID = fopen([FileOutputName '_HeaderPos.txt'],'w');
-    % for I_t = 1:length(HeaderPosition)
-    %   fprintf(fileID,'%d\n',HeaderPosition(I_t));  
-    % end
-    % fclose(fileID);
+    % Write header positions in the text file-----------------------------------
+    filenamei = strcat(L0_1_dir, "3_HeaderPos/", current_bird, '_HeaderPos.txt');
+    writematrix(HeaderPosition', filenamei);
     
     %Get tempterature and pressure---------------------------------------------
     [ T, P ] = GetCalibTemperaturePressureBMP388( Temp_t, Pressure_t, Calibration );  %Celsius, Pascals
     
     % Extract sensor data------------------------------------------------------
+    ECG_t = ECG_t(:,1);
+    ECG = single(cast(ECG_t,'uint16'))*Parameters.Coef;
+    ECG = ECG - repmat(median(ECG),size(ECG,1),1);
     
-    EEG_t = EEG_t(:,1);
-    
-    EEG = single(cast(EEG_t,'uint16'))*Parameters.Coef;
-    EEG = EEG - repmat(median(EEG),size(EEG,1),1);
-    
-    plott(EEG, 600)
+    plott(ECG, 600)
     
     Accel = zeros(size(Accel_t),'int16');
     for I_t = 1:3
@@ -97,21 +92,23 @@ for i= 1:length(fileList)
         Accel_r = [ax_rotated, ay_rotated, az_rotated];
         Gyro_r  = [gx_rotated, gy_rotated, gz_rotated];
     
-    %% save sensor files and EEG file separately
+    %% save sensor files and ECG file separately
     
     SensorMat = [Accel_r, Magn, Gyro_r, T, P]; % 75 Hz
-    EEG = EEG; % 600 Hz
+    % ECG = ECG; % 600 Hz
     
-    sensorfilename = [bird,'_sensormatraw75hz.mat'];
-    eegfilename = [bird,'_eegmatraw600hz.mat'];
+    sensorfilename = strcat(L0_0_dir, "output_mat/", current_bird,'_sensormatraw75hz.mat');
     save(sensorfilename, 'SensorMat', '-v7.3')
-    save(eegfilename,'EEG','-v7.3')
+
+    eegfilename = strcat(L0_0_dir, "output_mat/", current_bird,'_eegmatraw600hz.mat');
+    save(eegfilename,'ECG','-v7.3')
     
-    writematrix(EEG, ['/Volumes/GoogleDrive/My Drive/Thorne Lab/THORNE_LAB/Data!/Conners_Bird_Island/2019_2020/Tag_Data/L0_raw-data/Neurologgers/L0_1_Raw_Decompressed_txt/2_EEG/',FileName, '_EEG.txt'], 'delimiter',',')
-    writematrix(SensorMat,['/Volumes/GoogleDrive/My Drive/Thorne Lab/THORNE_LAB/Data!/Conners_Bird_Island/2019_2020/Tag_Data/L0_raw-data/Neurologgers/L0_1_Raw_Decompressed_txt/1_SensorData/',FileName, '_SensorData.txt'], 'delimiter',',')
+    writematrix(ECG, strcat(L0_1_dir, "2_ECG/", current_bird, "_ECG.txt")); % 'delimiter',',')
+    writematrix(SensorMat,strcat(L0_1_dir, "1_SensorData/", current_bird, "_SensorData.txt")); % 'delimiter',',')
     
-    
-    clearvars -except OutputFolder fileList i CurrentPath
+
+    %%
+    clearvars -except computer fileList GD_dir L0_0_dir L0_1_dir location szn i
+
 end
 
-%%
