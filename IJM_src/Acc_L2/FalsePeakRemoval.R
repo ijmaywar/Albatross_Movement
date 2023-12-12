51aef5cb9f5de54e2a1bb6313fa5cda4cbbe2fb5
ijmaywar <ijmaywar@gmail.com>
2023-12-12 15:46:11 -0500

    Create FalsePeakRemoval.R

---
+++ IJM_src/Acc_L2/FalsePeakRemoval.R
@@ -0,0 +1,18 @@
+setwd("/Users/ian/Library/CloudStorage/GoogleDrive-ian.maywar@stonybrook.edu/.shortcut-targets-by-id/1-mLOKt79AsOpkCFrunvcUj54nuqPInxf/THORNE_LAB/Data/Albatross/NEW_STRUCTURE/L2/Bird_Island/Tag_Data/Acc/2019_2020/")
+acc_files <- list.files(pattern='*.csv')
+
+for (i in 1:length(acc_files)) {
+  
+  # Read flap file
+  m <- read.csv(acc_files[i])
+  
+  # Explore histogram of flap heights
+  hist(m$pks)
+  
+  # This might not be too useful because this is filtered data.
+  # I should be looking at raw data to remove single point errors right? 
+  
+  # use matlab to look histogram of raw data points. 
+  
+  
+}
