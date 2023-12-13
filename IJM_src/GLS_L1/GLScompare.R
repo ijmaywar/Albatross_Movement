880f64ec38c121aec2b6efda9f1c43e515b2a48e
ijmaywar <ijmaywar@gmail.com>
2023-12-13 14:30:34 -0500

    Update GLScompare.R

--- IJM_src/GLS_L1/GLScompare.R
+++ IJM_src/GLS_L1/GLScompare.R
@@ -24,9 +24,13 @@ GD_dir <- "/Users/ian/Library/CloudStorage/GoogleDrive-ian.maywar@stonybrook.edu
 GLS_dir <- paste0(GD_dir, "THORNE_LAB/Data/Albatross/NEW_STRUCTURE/L1/Bird_Island/Tag_Data/GLS/", szn, "/")
 HMM_dir <- paste0(GD_dir, "THORNE_LAB/Data/Albatross/NEW_STRUCTURE/L3/Bird_Island/Tag_Data/GPS/", interp, "/", spp, "/")
 
+setwd(HMM_dir)
+allGPSfiles <- list.files(pattern='*.csv')
+
 setwd(GLS_dir)
 allGLSfiles <- list.files(pattern='*.csv')
 GLSfiles <- allGLSfiles[grep(paste0("^", spp), allGLSfiles)]
+birdnames <- sub("_GLS_L1.csv$","",GLSfiles)
 
 # User functions ----------------------------------------------------------
 
@@ -34,8 +38,20 @@ GLSfiles <- allGLSfiles[grep(paste0("^", spp), allGLSfiles)]
 # Loop thru and process ---------------------------------------------------
 
 for (i in 1:length(GLSfiles)) {
-  m <- read.csv(GLSfiles[i])
+  # find GLS and HMM data for current bird
+  setwd(GLS_dir)
+  GLS_data <- read.csv(GLSfiles[i])
+  setwd(HMM_dir)
+  HMM_data <- read.csv(allGPSfiles[grep(birdnames[i], allGPSfiles)])
   
+  m <- HMM_data
+  
+  GLS_wet <- filter(GLS_data,state=="wet")
+  for (j in nrow(GLS_wet)) {
+    # for each "wet" the starttime and endtime must span certain intervals in m.
+    # all of those intervals must be marked as wet in m.
+  }
+    
 }
 
 
