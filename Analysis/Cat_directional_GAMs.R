e74e013804bca34974a9ce87c44dc98ca5170309
ijmaywar <ijmaywar@gmail.com>
2024-09-25 21:47:58 -0400

    Update Cat_directional_GAMs.R
    Changed to nb distribution

--- Analysis/Cat_directional_GAMs.R
+++ Analysis/Cat_directional_GAMs.R
@@ -11,12 +11,14 @@ GAM_list_wind_vel_kmh_cat <- list()
 for (spp in spp_vec) {
   
   m_current <- m_all %>% filter((HMM_3S_state != 1) & (Species == spp)) %>% 
-    drop_na(wind_vel_kmh,bird_wind_angle_cat)
+    drop_na(flaps,wind_vel_kmh,shts,bird_wind_angle,bird_swell_angle,bird_wind_angle_cat,bird_swell_angle_cat)
   
-  current_GAM <- gam(formula = flaps ~ s(wind_vel_kmh,bird_wind_angle_cat,bs='fs',k=3) +
+  current_GAM <- gam(formula = flaps ~ s(wind_vel_kmh,bs='tp',k=3) +
+                       bird_wind_angle_cat + 
+                       s(wind_vel_kmh,by=bird_wind_angle_cat,bs='tp',k=3) +
                        s(id,k=length(unique(m_current$id)),bs="re"),
                      data = m_current,
-                     family = "poisson",
+                     family = "nb",
                      method = "REML")
   
   GAM_list_wind_vel_kmh_cat[[spp]] <- current_GAM
@@ -28,12 +30,9 @@ for (spp in spp_vec) {
   link_df <- cbind(current_ds,
                    rep(spp,nrow(current_ds)),
                    fitted_values(current_GAM, data = current_ds, scale = "link",
-                                 terms = c("(Intercept)","s(wind_vel_kmh,bird_wind_angle_cat)","s(id)"))[,4:7],
-                   fitted_values(current_GAM, data = current_ds, scale = "link",
-                                 terms = c("(Intercept)","s(wind_vel_kmh,bird_wind_angle_cat)"))[,4:7])
+                                 terms = c("(Intercept)",smooths(current_GAM)[1:4]))[,4:7])
   
   colnames(link_df) <- c("wind_vel_kmh","id","bird_wind_angle_cat","Species",
-                         "fitted_all","se_all","lower_all","upper_all",
                          "fitted_global","se_global","lower_global","upper_global")
   
   if (spp == "Black-browed") {
@@ -45,6 +44,7 @@ for (spp in spp_vec) {
 
 fv_df_wind_vel_kmh_cat$Species <- factor(fv_df_wind_vel_kmh_cat$Species,
                                          levels=spp_vec)
+fv_df_wind_vel_kmh_cat$bird_wind_angle_cat
 
 dir_cat_cols <- c("#9484B1FF", "#F1C100FF","#496849FF")
 fig_wind_cat <- ggplot(fv_df_wind_vel_kmh_cat) +
@@ -58,8 +58,7 @@ fig_wind_cat <- ggplot(fv_df_wind_vel_kmh_cat) +
   scale_fill_manual(values=dir_cat_cols,
                      labels=c("Head","Cross","Tail")) + 
   labs(y="Flaps/hour") +
-  # xlim(0,25) + 
-  # ylim(0,1500) +
+  ylim(0,2500) +
   facet_wrap(~Species,nrow = 1) + 
   theme_linedraw() +
   theme(axis.title.x = element_blank(),
@@ -69,12 +68,6 @@ fig_wind_cat <- ggplot(fv_df_wind_vel_kmh_cat) +
 
 fig_wind_cat
 
-for (i in 1:5) {
-  summary_i <- summary(GAM_list_wind_vel_kmh_cat[[i]])
-  print(c(as.numeric(format(round(summary_i$dev.expl,3),scientific=F)),
-          as.numeric(round(summary_i$r.sq,3)),
-          as.numeric(round(AIC(GAM_list_wind_vel_kmh_cat[[i]]),3))))
-}
 
 # s(shts,bird_swell_angle_cat), s(id) ------------------------------------------
 
@@ -83,12 +76,14 @@ GAM_list_shts_cat <- list()
 for (spp in spp_vec) {
   
   m_current <- m_all %>% filter((HMM_3S_state != 1) & (Species == spp)) %>% 
-    drop_na(shts,bird_swell_angle_cat)
+    drop_na(flaps,wind_vel_kmh,shts,bird_wind_angle,bird_swell_angle,bird_wind_angle_cat,bird_swell_angle_cat)
   
-  current_GAM <- gam(formula = flaps ~ s(shts,bird_swell_angle_cat,bs='fs',k=3) +
+  current_GAM <- gam(formula = flaps ~ s(shts,bs='tp',k=3) +
+                       bird_swell_angle_cat + 
+                       s(shts,by=bird_swell_angle_cat,bs='tp',k=3) +
                        s(id,k=length(unique(m_current$id)),bs="re"),
                      data = m_current,
-                     family = "poisson",
+                     family = "nb",
                      method = "REML")
   
   GAM_list_shts_cat[[spp]] <- current_GAM
@@ -100,12 +95,9 @@ for (spp in spp_vec) {
   link_df <- cbind(current_ds,
                    rep(spp,nrow(current_ds)),
                    fitted_values(current_GAM, data = current_ds, scale = "link",
-                                 terms = c("(Intercept)","s(shts,bird_swell_angle_cat)","s(id)"))[,4:7],
-                   fitted_values(current_GAM, data = current_ds, scale = "link",
-                                 terms = c("(Intercept)","s(shts,bird_swell_angle_cat)"))[,4:7])
+                                 terms = c("(Intercept)",smooths(current_GAM)[1:4]))[,4:7])
   
   colnames(link_df) <- c("shts","id","bird_swell_angle_cat","Species",
-                         "fitted_all","se_all","lower_all","upper_all",
                          "fitted_global","se_global","lower_global","upper_global")
   
   if (spp == "Black-browed") {
@@ -125,12 +117,12 @@ fig_swells_cat <- ggplot(fv_df_shts_cat) +
                             override.aes = list(fill = dir_cat_cols)),
          fill="none") +
   scale_color_manual(values=dir_cat_cols,
-                     labels=c("Head","Cross","Tail")) + 
+                     labels=c("Against","Across","With")) + 
   scale_fill_manual(values=dir_cat_cols,
-                    labels=c("Head","Cross","Tail")) + 
+                    labels=c("Against","Across","With")) + 
   labs(y="Flaps/hour") +
   # xlim(0,25) + 
-  # ylim(0,1500) +
+  ylim(0,2500) +
   facet_wrap(~Species,nrow = 1) + 
   theme_linedraw() +
   theme(axis.title.x = element_blank(),
@@ -140,9 +132,34 @@ fig_swells_cat <- ggplot(fv_df_shts_cat) +
 
 fig_swells_cat
 
-for (i in 1:5) {
-  summary_i <- summary(GAM_list_shts_cat[[i]])
-  print(c(as.numeric(format(round(summary_i$dev.expl,3),scientific=F)),
-          as.numeric(round(summary_i$r.sq,3)),
-          as.numeric(round(AIC(GAM_list_shts_cat[[i]]),3))))
+wrap_elements(panel = fig_wind_cat / fig_swells_cat)
+# 1000 x 500
+
+# summaries
+for (spp in spp_vec) {
+  
+  spp_AICs <- AICctab(GAM_list_wind_vel_kmh_cat[[spp]],GAM_list_shts_cat[[spp]],
+                          weights = TRUE, delta = TRUE, base=TRUE, sort = FALSE)
+  
+  spp_r.sq <- c(summary(GAM_list_wind_vel_kmh_cat[[spp]])$r.sq,summary(GAM_list_shts_cat[[spp]])$r.sq)
+  spp_dev.expl <- c(summary(GAM_list_wind_vel_kmh_cat[[spp]])$dev.expl,summary(GAM_list_shts_cat[[spp]])$dev.expl)
+  
+  spp_metrics <- as.data.frame(spp_AICs)
+  spp_metrics$r.sq <- spp_r.sq
+  spp_metrics$dev.expl <- spp_dev.expl
+  spp_metrics$spp <- spp
+  
+  if (spp == "Black-browed") {
+    All_metrics <- spp_metrics
+  } else {
+    All_metrics <- rbind(All_metrics,spp_metrics)
+  }
+  
 }
+
+All_metrics
+
+clipr::write_clip(All_metrics)
+
+
+
