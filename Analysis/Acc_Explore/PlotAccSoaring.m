5b49b9b2b7fa2de2209f108a50a0a0eb59c9cc5b
ijmaywar <ijmaywar@gmail.com>
2024-10-15 14:41:15 -0400

    Updating PlotAccSoaring

--- Analysis/Acc_Explore/PlotAccSoaring.m
+++ Analysis/Acc_Explore/PlotAccSoaring.m
@@ -33,37 +33,42 @@ p = get(0, "MonitorPositions");
 % Create Parameters folder
 mkdir(strcat(L2_dir,"Parameters/"))
 
+%% Find bird
+bird = "GHAL_20191218_Y239";
+j = find(strcmp(string(L1_fileList.name), strcat(bird,"_Acc_L1.csv")));
+if isempty(j)
+    disp('Bird not found.');
+end
+
 %% read file
-j = 27;
-bird = strsplit(L1_fileList.name{j},"_");
-bird = strcat(bird{1},"_",bird{2},"_",bird{3});
 Acc = readtable(L1_fileList.name{j},'Delimiter',',','ReadVariableNames',true,'Format','auto','TreatAsEmpty',{'NA'}); % read bird i's file
 Acc.DateTime.Format = 'yyyy-MM-dd HH:mm:ss.SSSSSS';
 
 %% Enter datetimes (copy and paste this from R code) and duration
-DS_dt = "2020-01-16 19:04:02";
-WSS_dt = "2020-01-17 05:04:02";
+adjust = minutes(10);
+DS_dt = datetime('2019-12-19 14:04:59', 'InputFormat', 'yyyy-MM-dd HH:mm:ss')+adjust;
+WSS_dt = datetime("2019-12-22 09:04:59", 'InputFormat', 'yyyy-MM-dd HH:mm:ss')+adjust;
 dur = minutes(2);
 
 % filter Acc data for DS and WSS
-DS_acc = Acc(Acc.DateTime >= DS_dt & Acc.DateTime <= DS_dt+dur);
-WSS_acc = Acc(Acc.DateTime >= WSS_dt & Acc.DateTime <= WSS_dt+dur);
+DS_acc = Acc(Acc.DateTime >= DS_dt & Acc.DateTime <= (DS_dt+dur),:);
+WSS_acc = Acc(Acc.DateTime >= WSS_dt & Acc.DateTime <= (WSS_dt+dur),:);
 
 %% Plot soaring behaviors
 
 figure
 subplot(2,1,1)
-plot(DS_acc.DateTime,DS_acc.___)
+plot(DS_acc.DateTime,DS_acc.Ax)
 hold on
-plot(DS_acc.DateTime,DS_acc.___)
+plot(DS_acc.DateTime,DS_acc.Ay)
 hold on 
-plot(DS_acc.DateTime,DS_acc.___)
+plot(DS_acc.DateTime,DS_acc.Az)
 
 subplot(2,1,2)
-plot(WSS_acc.DateTime,WSS_acc.___)
+plot(WSS_acc.DateTime,WSS_acc.Ax)
 hold on
-plot(WSS_acc.DateTime,WSS_acc.___)
+plot(WSS_acc.DateTime,WSS_acc.Ay)
 hold on 
-plot(WSS_acc.DateTime,WSS_acc.___)
+plot(WSS_acc.DateTime,WSS_acc.Az)
 
 
